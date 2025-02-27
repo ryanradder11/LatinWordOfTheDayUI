5afce0baa84f6c3ade7b41af3270b7c3097aa72b
ryanradder <ryan.radder@headfirst.nl>
2025-02-27 20:21:32 +0100

    generated environment

--- latinWordOfTheDay/src/app/app.module.ts
+++ latinWordOfTheDay/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CommonModule} from '@angular/common';
+import {provideHttpClient} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ latinWordOfTheDay/src/app/models/word-of-the-day.model.ts
@@ -0,0 +1,11 @@
+export interface WordOfTheDay {
+  id: number;
+  word: string;
+  definition: string;
+  pronunciation: string;
+  origin: string;
+  example: string;
+  synonyms: string[];
+  antonyms: string[];
+  image: string;
+}
