13c2a351cb27b6d9139b3f96d467cde4fceb30f3
ryanradder <ryan.radder@headfirst.nl>
2025-02-27 21:49:07 +0100

    -angular 18 update

--- latinWordOfTheDay/src/app/app.module.ts
+++ latinWordOfTheDay/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {CommonModule} from '@angular/common';
-import {provideHttpClient} from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
--- latinWordOfTheDay/src/app/services/word-of-the-day.service.ts
+++ latinWordOfTheDay/src/app/services/word-of-the-day.service.ts
@@ -1,7 +1,7 @@
 import {inject, Injectable} from '@angular/core';
 import {WordOfTheDay} from '../models/word-of-the-day.model';
 import {Observable} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import {environment} from '../../environments/environment';
 
 @Injectable({
