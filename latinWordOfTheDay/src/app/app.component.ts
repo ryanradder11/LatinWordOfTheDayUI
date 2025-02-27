eb4f7bf0d4f3027bdf621e0447346a9cc0bc3df5
ryanradder <ryan.radder@headfirst.nl>
2025-02-27 21:20:12 +0100

    some styling

--- latinWordOfTheDay/src/app/app.component.ts
+++ latinWordOfTheDay/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {of, Observable} from 'rxjs';
+import {WordOfTheDay} from './models/word-of-the-day.model';
+import {WordOfTheDayService} from './services/word-of-the-day.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'latinWordOfTheDay';
+  private wordOfTheDayService = inject(WordOfTheDayService);
+
+  public wordOfTheDay$: Observable<WordOfTheDay | null>=  of(null);
+
+  public ngOnInit() {
+    this.wordOfTheDay$ = this.wordOfTheDayService.getWordOfTheDay();
+  }
 }
--- latinWordOfTheDay/src/app/services/word-of-the-day.service.ts
+++ latinWordOfTheDay/src/app/services/word-of-the-day.service.ts
@@ -2,6 +2,7 @@ import {inject, Injectable} from '@angular/core';
 import {WordOfTheDay} from '../models/word-of-the-day.model';
 import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,6 @@ export class WordOfTheDayService {
   private http = inject(HttpClient);
 
   public getWordOfTheDay(): Observable<WordOfTheDay> {
-    return this.http.get<WordOfTheDay>('https:///jokes/random');
+    return this.http.get<WordOfTheDay>(`${environment.apiUrl}/items`);
   }
 }
