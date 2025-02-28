7eb0160d08a4cc29959f825f26b3d2d601330d32
ryanradder <ryan.radder@headfirst.nl>
2025-02-28 15:50:45 +0100

    -added scroll component

---
+++ latinWordOfTheDay/src/app/scroll/scroll.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input} from '@angular/core';
+import {WordOfTheDay} from "../models/word-of-the-day.model";
+import {CapitalizePipe} from "../pipes/capitalize.pipe";
+
+@Component({
+  selector: 'app-scroll',
+  standalone: true,
+  imports: [CapitalizePipe],
+  templateUrl: './scroll.component.html',
+  styleUrl: './scroll.component.scss'
+})
+export class ScrollComponent {
+
+  @Input({ required: true }) wordOfTheDay!: WordOfTheDay;
+
+}
