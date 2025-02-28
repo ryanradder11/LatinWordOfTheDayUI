75c8cd4eb3f21780e9629d8409cc63be400f7836
ryanradder <ryan.radder@headfirst.nl>
2025-02-28 23:02:26 +0100

    -new styling -3 examples english/latin split

--- latinWordOfTheDay/src/app/models/word-of-the-day.model.ts
+++ latinWordOfTheDay/src/app/models/word-of-the-day.model.ts
@@ -4,7 +4,12 @@ export interface WordOfTheDay {
   definition: string;
   pronunciation: string;
   origin: string;
-  example: string;
+  example0: string;
+  example0Latin: string;
+  example1: string;
+  example1Latin: string;
+  example2: string;
+  example2Latin: string;
   synonyms: string[];
   antonyms: string[];
   image: string;
