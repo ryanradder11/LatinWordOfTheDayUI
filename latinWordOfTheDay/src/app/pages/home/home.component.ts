0ef347d685851701f8e2b8db4425cefef7e9ba68
ryanradder <ryan.radder@headfirst.nl>
2025-02-28 18:45:19 +0100

    -added pages

---
+++ latinWordOfTheDay/src/app/pages/home/home.component.ts
@@ -0,0 +1,21 @@
+import {Component, inject} from '@angular/core';
+import {Observable} from "rxjs";
+import {WordOfTheDay} from "../../models/word-of-the-day.model";
+import {WordOfTheDayService} from "../../services/word-of-the-day.service";
+import {ScrollComponent} from "../../components/scroll/scroll.component";
+import {AsyncPipe} from "@angular/common";
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    ScrollComponent,
+    AsyncPipe
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  private wordOfTheDayService = inject(WordOfTheDayService);
+
+  public wordOfTheDay$: Observable<WordOfTheDay> = this.wordOfTheDayService.getWordOfTheDay();
+}
