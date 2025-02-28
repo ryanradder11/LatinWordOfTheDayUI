0ebd10628bb34a3d4d80784b5797b8d170f05e75
ryanradder <ryan.radder@headfirst.nl>
2025-02-28 23:13:10 +0100

    -added why component

---
+++ latinWordOfTheDay/src/app/pages/why/why.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-why',
+  imports: [],
+  templateUrl: './why.component.html',
+  styleUrl: './why.component.scss'
+})
+export class WhyComponent {
+
+}
