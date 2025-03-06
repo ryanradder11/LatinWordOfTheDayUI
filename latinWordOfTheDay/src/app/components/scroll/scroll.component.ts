ca06499f4c28c3f37bca94d1f5bdc6d7f3f5830e
ryanradder <ryan.radder@headfirst.nl>
2025-03-06 14:20:11 +0100

    -added hostListener for button behaviour

--- latinWordOfTheDay/src/app/components/scroll/scroll.component.ts
+++ latinWordOfTheDay/src/app/components/scroll/scroll.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, Input} from '@angular/core';
+import {Component, inject, Input, HostListener} from '@angular/core';
 import {CapitalizePipe} from "../../pipes/capitalize.pipe";
 import {Store} from "@ngrx/store";
 import {toggleFavorite} from "../../store/word-of-the-day.actions";
@@ -30,5 +30,15 @@ export class ScrollComponent {
 
     // Speak the text
     speechSynthesis.speak(utterance);
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent) {
+    if (event.key === 's' || event.key === 'S') {
+      this.speak();
+    }
+    if(event.key === 'f' || event.key === 'F') {
+      this.toggleFavorite();
+    }
   }
 }
